fc40f2540eaa7511a99cc87a091df3e787a30b20
habdelgawad1 <ha2400611@tkh.edu.eg>
2026-03-18 22:40:07 +0200

    Multithreading added

--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <pthread.h>       // Threads
 #include <sys/socket.h>    // Unix socket API
 #include <netinet/in.h>    // Unix network structures  
 #include <arpa/inet.h>     // Unix address conversion
@@ -8,12 +9,55 @@
 #include "security.h"      
 using namespace std;
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* handle_client(void* arg){
+    int client = (intptr_t)arg;
+
+    DiffieHellman dh;
+    XORCipher cipher;
+
+    // Diffie-Hellman Key Exchange
+    dh.generateKeys();
+
+    // Send public key to client
+    string my_key = to_string(dh.getPublicKey()) + "\n";
+    send(client, my_key.c_str(), my_key.length(), 0);
+
+    // Receive client's public key
+    char buffer[BUFFER_SIZE];
+    recv(client, buffer, BUFFER_SIZE, 0);
+    dh.computeSharedSecret(stoll(string(buffer)));
+    cipher.setKey(dh.getSharedSecret());
+
+    // Secure Command Reception Loop
+    while (true) {
+        int len = recv(client, buffer, BUFFER_SIZE, 0);
+        if (len <= 0) break; 
+
+        // Null-terminate the received data
+        buffer[len] = 0;
+
+        string hex(buffer);                           
+        string encrypted = cipher.fromHex(hex);       
+        string command = cipher.encrypt(encrypted);   
+
+        //Lock mutex to safely print command
+        pthread_mutex_lock(&mutex);
+        cout << "Command: " << command << endl;
+        pthread_mutex_unlock(&mutex);
+
+    }
+
+    // Clean up client connection
+    close(client);
+    return nullptr;
+}
 int main() {
     // Create server socket
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     
     // Configure server address structure
-    // INADDR_ANY means accept connections on any network interface
     sockaddr_in addr = {AF_INET, htons(SERVER_PORT), {INADDR_ANY}};
     
     // Bind socket to the specified port and start listening
@@ -26,45 +70,9 @@ int main() {
         int client = accept(server_socket, 0, 0);
         cout << "Client connected" << endl;
         
-        DiffieHellman dh;    
-        XORCipher cipher;  
-        
-        // Diffie-Hellman Key Exchange 
-        dh.generateKeys();
-        
-        // Send public key to client
-        string my_key = to_string(dh.getPublicKey()) + "\n";
-        send(client, my_key.c_str(), my_key.length(), 0);
-        
-        // Receive client's public key
-        char buffer[BUFFER_SIZE];
-        recv(client, buffer, BUFFER_SIZE, 0);
-        long long client_key = stoll(string(buffer));
-        
-        dh.computeSharedSecret(client_key);
-        cipher.setKey(dh.getSharedSecret());
-        
-        // Secure Command Reception Loop
-        while (true) {
-            int len = recv(client, buffer, BUFFER_SIZE, 0);
-            if (len <= 0) break; 
-            
-            // Null-terminate the received data
-            buffer[len] = 0;
-            
-            string hex(buffer);                           
-            string encrypted = cipher.fromHex(hex);       
-            string command = cipher.encrypt(encrypted);   
-            
-            cout << "Command: " << command << endl;
-        }
-        
-        // Clean up client connection 
-        close(client);
-        cout << "Client disconnected" << endl;
+        pthread_t thread_id;
+        pthread_create(&thread_id, nullptr, handle_client, (void*)(intptr_t)client);
+        // Detach thread to allow independent execution
+        pthread_detach(thread_id);
     }
-    
-    // Clean up server socket 
-    close(server_socket);
-    return 0;
 }
